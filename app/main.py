afc5eabcc532840a5839fba386d2b5d21ead0f5e
Brandon Chen <chen@bochen.dev>
2024-09-14 20:43:12 +0800

    feat: send 200 success response when receive request

--- app/main.py
+++ app/main.py
@@ -6,7 +6,25 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+
+    """
+    // Status line
+    HTTP/1.1  // HTTP version
+    200       // Status code
+    OK        // Optional reason phrase
+    \r\n      // CRLF that marks the end of the status line
+
+    // Headers (empty)
+    \r\n      // CRLF that marks the end of the headers
+
+    // Response body (empty)
+    """
+    status_line = b"HTTP/1.1 200 OK\r\n"
+    headers = b"\r\n"
+    body = b""
+    http_response = status_line + headers + body
+    connection.sendall(http_response)
 
 
 if __name__ == "__main__":
